10eaee92a1a81c6a0278da21609028f476d3eb06
Adam Krull <aqkrull@gmail.com>
2023-08-11 09:50:05 -0500

    updated with decision tree lesson

--- adam_prepare.py
+++ adam_prepare.py
@@ -6,7 +6,7 @@ from adam_acquire import get_titanic
 
 def drop_cols(df):
     
-    return df.drop(columns = ['pclass', 'passenger_id', 'embarked'])
+    return df.drop(columns = ['pclass', 'passenger_id', 'embarked', 'deck'])
 
 
 def train_val_test(df, strat, seed = 42):
